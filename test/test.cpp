e371feb864d153ab092ce412e3b9bb8a5d0828df
Furkan Özen <furkanoozen4@gmail.com>
2025-04-12 08:24:30 +0000

    Test files are added

--- test/test.cpp
+++ test/test.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include "CANFD.h"
 
 int toplama(int a, int b){
     return a+b;
@@ -10,6 +11,33 @@ TEST(Toplama_Testi, Pozifdifgsg){
 
 
 int main(){
+
+
+    // In the receiver side 
+    // sudo ip link add dev "Network name" type vcan
+    // sudo ip link set up "Network name"
+
+    // candump "Network name"
+
+
+    // In the sender side 
+    // cansend "Network Name" 123#DEADBEEF
+
+
+    CANFD my_CANFD;
+
+    bool zort = my_CANFD.CreateSocket("FurkanSocket","FurkanInterface",false,true);
+    std::cout<<" return deger"<< zort;
+    while(1){
+
+    };
+
+    /*
     ::testing::InitGoogleTest();
     return RUN_ALL_TESTS();
+    */
+
+
+
+
 }
